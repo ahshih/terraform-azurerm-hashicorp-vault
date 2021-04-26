292b45a01fd9992442ba2600d0ad352a56f2f2fd
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-04-26 17:42:13 -0400

    add key vault outputs

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "key_vault_id" {
+  description = "The ID of the Key Vault."
+  value       = azurerm_key_vault.kv.id
+}
+
+output "key_vault_uri" {
+  description = "The URI of the Key Vault, used for performing operations on keys and secrets." 
+  value       = azurerm_key_vault.kv.uri
+}
+
+output "vault_init_secret_uri" {
+  description = "The Azure Key Vault URI of the Hashicorp Vault init information."
+  value       = "${azurerm_key_vault.kv.uri}/secrets/hashicorp-vault-init"
+}
