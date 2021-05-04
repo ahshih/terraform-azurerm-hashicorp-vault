4e4a0ddd981f76fe31b09bb086463155dcf01bf5
Albert Shih <Albert.Shih@neudesic.com>
2021-05-04 12:25:13 -0700

    change to principal id

--- main.tf
+++ main.tf
@@ -123,14 +123,14 @@ resource "azurerm_key_vault_access_policy" "vault_init" {
 module "vault_identity" {
   source = "github.com/Azure-Terraform/terraform-azurerm-kubernetes-aad-pod-identity?ref=v2.0.1"
   
-  aks_identity         = azurerm_user_assigned_identity.vault.id
+  aks_identity         = azurerm_user_assigned_identity.vault.principal_id
   aks_node_resource_group = var.resource_group_name
 }
 
 module "vault_init_identity" {
   source = "github.com/Azure-Terraform/terraform-azurerm-kubernetes-aad-pod-identity?ref=v2.0.1"
 
-  aks_identity         = azurerm_user_assigned_identity.vault.id
+  aks_identity         = azurerm_user_assigned_identity.vault.principal_id
   aks_node_resource_group = var.resource_group_name
 }
 
